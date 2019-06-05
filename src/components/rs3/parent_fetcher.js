1cb9aecf3a5ab4fda1d7073abba39a94d869e476
Matthew Hughes <matthew.l.hughes@kp.org>
2019-06-05 15:28:00 -0700

    added a redirects folder for netlify deployment

--- src/components/rs3/parent_fetcher.js
+++ src/components/rs3/parent_fetcher.js
@@ -1,111 +1,109 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import FetchStats from "./fetch_rs3_stats";
 import FetchMinigames from "./fetch_rs3_minigames";
 import FetchActivityLog from "./fetch_activity_log";
 import FetchUserData from "./fetch_user_data";
 import NameForm from "../NameForm";
-import Img from 'react-image';
-import Tabs from 'react-bootstrap/Tabs';
-import Tab from 'react-bootstrap/Tab';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import { Link } from 'react-router-dom'
+import Img from "react-image";
+import Tabs from "react-bootstrap/Tabs";
+import Tab from "react-bootstrap/Tab";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 export default class parent_fetcher extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { user: 'zee pk' };
-        this.render = this.render.bind(this);
-        this.username = this.state.user.replace(' ', '+');
-    }
+  constructor(props) {
+    super(props);
+    this.state = { user: "zee pk" };
+    this.render = this.render.bind(this);
+    this.username = this.state.user.replace(" ", "+");
+  }
 
-    nameWithPluses() {
-        var username = this.state.user.replace(' ', '+');
-        username = username.replace('_', '+');
-        return username;
-    }
+  nameWithPluses() {
+    var username = this.state.user.replace(" ", "+");
+    username = username.replace("_", "+");
+    return username;
+  }
 
-    nameWithSpaces() {
-        var username = this.state.user.replace('+', ' ');
-        username = username.replace('_', ' ');
-        return username;
-    }
-    //testing git
+  nameWithSpaces() {
+    var username = this.state.user.replace("+", " ");
+    username = username.replace("_", " ");
+    return username;
+  }
+  //testing git
 
-    render() {
-        return (
+  render() {
+    return (
+      <div>
+        <Navbar id="topnav" expand="lg">
+          <a className="navbar-brand" href=" ">
+            <img
+              id="logo"
+              src={require("../../woodcutLogo.png")}
+              alt="Site Logo"
+            />
+          </a>
+
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#">RS3</Nav.Link>
+              <Nav.Link href="/osrs">osrs</Nav.Link>
+            </Nav>
+            {/* search goes here */}
+            <NameForm
+              user={this.state.user}
+              changeName={user => this.setState({ user })}
+            />
+          </Navbar.Collapse>
+
+          {/* </div> */}
+        </Navbar>
+
+        {/* <!-- GRID --> */}
+
+        <div className="grid-container">
+          {/* stat table */}
+
+          <div className="grid-item" id="stat-table">
+            {/* stat table component goes here */}
+            <br />
+            <br />
+            <FetchStats user={this.state.user} />
+          </div>
+
+          {/* player info */}
+          <div className="grid-item" id="player-info">
             <div>
-                <Navbar id="topnav" expand="lg">
-                    <a className="navbar-brand" href=" ">
-                        <img id="logo" src={require("../../woodcutLogo.png")} alt="Site Logo" />
-                    </a>
-
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                    <Nav.Link href="#">RS3</Nav.Link>
-                    <Nav.Link href="/osrs">osrs</Nav.Link>
-
-                        </Nav>
-                        {/* search goes here */}
-                        <NameForm user={this.state.user} changeName={(user) => this.setState({ user })} />
-                    </Navbar.Collapse>
-
-
-
-                    {/* </div> */}
-                </Navbar>
-
-                {/* <!-- GRID --> */}
-
-                <div className="grid-container">
-                    {/* stat table */}
-
-                    <div className="grid-item" id="stat-table">
-
-                        {/* stat table component goes here */}
-                        <br/><br/>
-                        <FetchStats user={this.state.user} />
-
-                    </div>
-
-
-                    {/* player info */}
-                    <div className="grid-item" id="player-info">
-                        <div>
-                            {/* avatar image */}
-                            <Img src={"http://secure.runescape.com/m=avatar-rs/" + this.nameWithPluses() + "/chat.png"} />
-                        </div>
-                        <div>
-                            {/* username */}
-                            <h1 id="username">{this.nameWithSpaces()}</h1>
-                        </div>
-                        {/* player grid */}
-                     
-
-                            {/* user info component goes here */}
-                            <FetchUserData user={this.state.user} />
-                        
-                    </div>
-                    {/* activity table component goes here */}
-                    <div className="grid-item">
-                    <Tabs defaultActiveKey="activity" id="uncontrolled-tab-example">
-                    <Tab eventKey="activity" title="Activity">
-                        <FetchActivityLog user={this.state.user} />
-                        </Tab>
-                        <Tab eventKey="minigames" title="Minigame Stats">
-                        <FetchMinigames user={this.state.user} />                        
-                        </Tab>
-                    </Tabs>    
-                    </div>
-                </div>
-
-
-                {/* <h1>{this.state.user}</h1> */}
-
-
+              {/* avatar image */}
+              <Img
+                src={
+                  "http://secure.runescape.com/m=avatar-rs/" +
+                  this.nameWithPluses() +
+                  "/chat.png"
+                }
+              />
             </div>
-        )
-    }
+            <div>
+              {/* username */}
+              <h1 id="username">{this.nameWithSpaces()}</h1>
+            </div>
+            {/* player grid */}
+
+            {/* user info component goes here */}
+            <FetchUserData user={this.state.user} />
+          </div>
+          {/* activity table component goes here */}
+          <div className="grid-item">
+            <Tabs defaultActiveKey="activity" id="uncontrolled-tab-example">
+              <Tab eventKey="activity" title="Activity">
+                <FetchActivityLog user={this.state.user} />
+              </Tab>
+              <Tab eventKey="minigames" title="Minigame Stats">
+                <FetchMinigames user={this.state.user} />
+              </Tab>
+            </Tabs>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-
